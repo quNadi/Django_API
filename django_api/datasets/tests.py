33f15060a3d6f85189730e6a01df488191238d85
ullu <ula.lukierska@gmail.com>
2021-01-01 01:51:21 +0100

    django-pytest cz1

--- django_api/datasets/tests.py
+++ django_api/datasets/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.utils.http import urlencode
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from datasets.models import DataCat
+from datasets import views
+
+class DataCatTest(APITestCase):
+    def post_category(self,kind):
+        url=reverse(views.DataCatList.name)
+        data={"kind":kind}
+        response=self.client.post(url,data,format='json')
+        return response
+
+    def test_post_get_category(self):
+        new_cat='newtest'
+        response=self.post_category(new_cat)
+        print("pk {0}".format(DataCat.objects.get().pk))
+        assert DataCat.objects.count() ==1
+        assert DataCat.objects.get().name== new_cat
