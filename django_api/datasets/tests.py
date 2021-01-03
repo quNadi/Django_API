a02103fe92d32c9e5e3b8855b7a8d703fc7cf725
ullu <ula.lukierska@gmail.com>
2021-01-04 00:17:25 +0100

    django-pytest cz4 + authtoken check

--- django_api/datasets/tests.py
+++ django_api/datasets/tests.py
@@ -7,6 +7,10 @@ from rest_framework.test import APITestCase
 from datasets.models import DataCat
 from datasets import views
 
+from datasets.models import Researcher
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 class DataCatTest(APITestCase):
     def post_category(self,kind):
         url=reverse(views.DataCatList.name)
@@ -62,3 +66,52 @@ class DataCatTest(APITestCase):
         patch_res=self.client.patch(url,data,format="json")
         assert patch_res.status_code==status.HTTP_200_OK
         assert patch_res.data['kind']==update_cat
+
+
+    def test_get_cat(self):
+        new_cat="easy"
+        res=self.post_category(new_cat)
+        url=reverse(views.DataCatDetail.name,None,{res.data['pk']})
+        get_res=self.client.get(url,format='json')
+        assert get_res.status_code==status.HTTP_200_OK
+        assert get_res.data['kind']==new_cat
+
+
+#####################authtoken
+
+class ResearcherTest(APITestCase):
+
+    def post_researcher(self, name,origin):
+        url= reverse(views.ResearcherList.name)
+        data={
+            'name':name,
+            'origin':origin,
+        }
+        response=self.client.post(url,data,format='json')
+        return response
+
+    def create_user_and_token(self):
+        user=User.objects.create_user('user02','user02@gmail.com','password013D')
+        token=Token.objects.create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token {0}'.format(token.key))
+
+
+    def test_post_get_researcher(self):
+
+        self.create_user_and_token()
+        new_name='zoska'
+        new_origin=Researcher.EUROPEAN
+        res=self.post_researcher(new_name,new_origin)
+
+        assert res.status_code==status.HTTP_201_CREATED
+        assert Researcher.objects.count()==1
+        saved_res=Researcher.objects.get()
+        assert saved_res.name==new_name
+        url=reverse(views.ResearcherDetail.name,None,{saved_res.pk})
+        authoriz_get=self.client.get(url,format='json')
+        assert authoriz_get.status_code==status.HTTP_200_OK
+        assert authoriz_get.data['name']==new_name
+        self.client.credentials()
+        unautoriz_get=self.client.get(url,format='json')
+        assert unautoriz_get.status_code==status.HTTP_401_UNAUTHORIZED
+
